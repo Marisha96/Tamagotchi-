cf5bb396481e8f2763c2a5e116fad6689e6fb964
Marisha96 <Marisha.glasgow@yahoo.com>
2022-03-10 09:58:57 -0500

    s

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const myHungry = document.getElementById("my-hungry");
- const myPlay = document.getElementById("my-play");
+// let myName = window.prompt("enter name please?");
+// console.log("Hello", myName );
+// document.getElementById("myButton").onclick = function() {
+//     let myName = document.getElementById("myText").value;
+//     console.log("Hello", myName);
+// }
 
+
+const myHungry = document.getElementById("my-hungry");
+//  const myPlay = document.getElementById("my-play");
 
 const myHunger = document.getElementById("my-hunger");
 console.log(myHunger.innerText);
-
 
 
 
@@ -15,7 +21,7 @@ const addOne = () => {
 // console.log("addTwo function invoked")
 num++;
 myHunger.innerText = num;
-// console.log(num);
+console.log(num);
 };
 
 myHungry.addEventListener('click', addOne);
@@ -23,14 +29,15 @@ myHungry.addEventListener('click', addOne);
 
 const myFeed = document.getElementById("my-feed");
 
-const mySleepinees = document.getElementById("my-sleepiness")
+const mySleepiness = document.getElementById("my-sleepiness")
 console.log(myFeed.innerText);
 
 
 let count = 2 
 const addThree = () => {
     count++
-    myFeed.innerText = count
+    myFeed.innerText = count;
+    console.log(count);
 
 };
 
