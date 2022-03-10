b377ee2919ca6f98e16993e645111464380e6c56
Marisha96 <Marisha.glasgow@yahoo.com>
2022-03-09 22:18:33 -0500

    h

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
-
 const myHungry = document.getElementById("my-hungry");
- 
+ const myPlay = document.getElementById("my-play");
+
 
 const myHunger = document.getElementById("my-hunger");
 console.log(myHunger.innerText);
 
+
+
+
 let num = 0;
 
 
-
-
-const addTwo = () => {
+const addOne = () => {
 // console.log("addTwo function invoked")
 num++;
 myHunger.innerText = num;
-console.log(num);
+// console.log(num);
 };
 
-myHungry.addEventListener('click', addTwo);
+myHungry.addEventListener('click', addOne);
 
 
+const myFeed = document.getElementById("my-feed");
+
+const mySleepinees = document.getElementById("my-sleepiness")
+console.log(myFeed.innerText);
+
+
+let count = 2 
+const addThree = () => {
+    count++
+    myFeed.innerText = count
+
+};
+
+myFeed.addEventListener('click',addThree);
