838509345c2654e9aa3d66931ac4f1becdf80bd4
Marisha96 <Marisha.glasgow@yahoo.com>
2022-03-10 23:09:32 -0500

    kdls

--- main.js
+++ main.js
@@ -30,3 +30,55 @@ const rest = document.getElementById("sleep")
 const bone = document.getElementById("play")
 const petAlive = document.getElementById("alive")
 
+// adding onclick function to each elements
+feedEl.addEventListener('click', feedClick);
+sleepEl.addEventListener('click', sleepClick);
+playEl.addEventListener('click', playClick);
+resetEl.addEventListener('click', resetClick);
+
+// function to increment the age
+function ageCount() {
+  ageCounter = setInterval(function () {
+      // increment the age of pet
+      agePet++;
+      age.innerText = "Age: " + agePet;
+      if (hunger == 10 || bore == 10 || sleep == 10)
+      // stop the timer if any of the above condition satsfies
+          clearInterval(ageCounter);
+  // time interval is set as 1 minute
+  }, 1000); 
+}
+
+// function to increment the hunger
+function hungerCount() {
+  hungerCounter = setInterval(function () {
+      hunger++;
+      hungry.innerText = "Hunger: " + hunger;
+      if (hunger == 10)
+          // if the value of bore reaches the value 10 first then display the message 
+          petAlive.innerText = "Your pet died of hunger. ";
+          document.getElementById("tam").src="images/cry.jpeg"
+          if (hunger == 10 || bore == 10 || sleep == 10)
+          // stop the timer if any of the above condition satsfies
+          clearInterval(hungerCounter);
+  // time interval is set as .5 seconds
+  }, 500);
+
+}
+
+// function to increment the boredom
+function boreCount() {
+  boreCounter = setInterval(function () {
+      bore++;
+      bone.innerText = "Bore: " + bore;
+      if (bore == 10)
+          // if the value of bore reaches the value 10 first then display the message 
+          document.getElementById("tam").src="images/sleepy.jpeg"
+          petAlive.innerText = "Your pet died of boredom. ";
+          
+          if (hunger == 10 || bore == 10 || sleep == 10)
+          // stop the timer if any of the above condition satsfies
+          clearInterval(boreCounter);
+  // time interval is set as 1 second
+  }, 1000);
+}
