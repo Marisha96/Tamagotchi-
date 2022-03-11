b763f74241958e42750df57cbe4056113bc934fa
Marisha96 <Marisha.glasgow@yahoo.com>
2022-03-10 23:08:49 -0500

    hdkj

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ let bore = 0
 let sleep = 0
 
 // calls each function when the page loads
+window.onload = function main() {
+  ageCount();
+  hungerCount();
+  boreCount();
+  sleepCount();
+}
+
+// added each variable for the set interval function so we can stop the timer
+// according to the condition we set
+let ageCounter = null;
+let hungerCounter = null
+let boreCounter = null
+let sleepCounter = null
+
+// accessing the DOM elements of html
+const feedEl = document.getElementById("btn-feed");
+const sleepEl = document.getElementById("btn-sleep");
+const playEl = document.getElementById("btn-play");
+const resetEl = document.getElementById("btn-reset");
+const age = document.getElementById("age")
+const hungry = document.getElementById("feed")
+const rest = document.getElementById("sleep")
+const bone = document.getElementById("play")
+const petAlive = document.getElementById("alive")
+
