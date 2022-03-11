0ce385954eebff2c71cd6665910b27a83f9e1beb
Marisha96 <Marisha.glasgow@yahoo.com>
2022-03-11 17:04:08 -0500

    Finale

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-let myName = window.prompt("enter name please?");
-console.log("Hello", myName );
-
-
+function myName() {
+  let tamaName = prompt("Please enter your name", "");
+  if (tamaName != null) {
+    document.getElementById("tama").innerHTML =
+      "Hello " + tamaName + "! Are you ready to plays?";
+  } 
+  main()
+} 
 let agePet = 0
 let hunger = 0
 let bore = 0
 let sleep = 0
 
 
-window.onload = function main() {
+
+function main() {
   ageCount();
   hungerCount();
   boreCount();
   sleepCount();
 }
+
 
 
 let ageCounter = null;
@@ -22,15 +28,19 @@ let boreCounter = null
 let sleepCounter = null
 
 
+
 const feedEl = document.getElementById("btn-feed");
 const sleepEl = document.getElementById("btn-sleep");
 const playEl = document.getElementById("btn-play");
 const resetEl = document.getElementById("btn-reset");
 const age = document.getElementById("age")
 const hungry = document.getElementById("feed")
+const rest = document.getElementById("sleep")
+const bone = document.getElementById("play")
 const sleepiness = document.getElementById("sleep")
 const boredom = document.getElementById("play")
 const petAlive = document.getElementById("alive")
+
 
 
 feedEl.addEventListener('click', feedClick);
@@ -39,68 +49,88 @@ playEl.addEventListener('click', playClick);
 resetEl.addEventListener('click', resetClick);
 
 
+
 function ageCount() {
   ageCounter = setInterval(function () {
-      //  this increment the age of pet
-      agePet++;
-      age.innerText = "Age: " + agePet;
-      if (hunger == 10 || bore == 10 || sleep == 10)
+    // increment the age of pet
+    //  this increment the age of pet
+    agePet++;
+    age.innerText = "Age: " + agePet;
+    if (hunger == 10 || bore == 10 || sleep == 10)
       // stop the timer if any of the above condition satsfies
-          clearInterval(ageCounter);
-  // time interval is set as 2 minutes
-  }, 2000); 
-}
-
-// / to increment the hunger
-function hungerCount() {
-  hungerCounter = setInterval(function () {
-      hunger++;
-      hungry.innerText = "Hunger: " + hunger;
-      if (hunger == 10)
-          // if the value of boredom reaches the value 10 first then display the message 
-          petAlive.innerText = " Tama died of hunger. ";
-          document.getElementById("tam").src="images/cry.jpeg"
-          if (hunger == 10 || bore == 10 || sleep == 10)
-          // stop the timer if any of the above condition satsfies
-          clearInterval(hungerCounter);
-  }, 1500);
-
-}
-
-
-function boreCount() {
-  boreCounter = setInterval(function () {
-      bore++;
-      boredom.innerText = "Boredom: " + bore;
-      if (bore == 6)
-          // if the value of bore reaches the value 10 first then display the message 
-          petAlive.innerText = "Tama died of boredom. ";
-         
-          if (hunger == 7 || bore == 10 || sleep == 4)
-          // stop the timer if any of the above condition satsfies
-          clearInterval(boreCounter);
-  // time interval is set as 1 minute
-  }, 1000);
-}
-
-
-function sleepCount() {
-  sleepCounter = setInterval(function () {
-      sleep++;
-      sleepiness.innerText = "Sleepiness: " + sleep;
-      if (sleep == 10) 
-      // if the value of bore reaches the value 10 first then display the message 
-     
-      petAlive.innerText = " Tama is tired.";
-      document.getElementById("tam").src="images/sleepy.jpeg"
-          if (hunger == 3 || bore ==  8 || sleep == 10)
-          
-          // stop the timer if any of the above condition satsfies
-          clearInterval(sleepCounter);
-  // time interval is set as 2 seconds
+      clearInterval(ageCounter);
+    // time interval is set as 1 minute
+    // time interval is set as 2 minutes
   }, 2000);
 }
 
+// function to increment the hunger
+// / to increment the hunger
+function hungerCount() {
+  hungerCounter = setInterval(function () {
+    hunger++;
+    hungry.innerText = "Hunger: " + hunger;
+    if (hunger == 10)
+      // if the value of bore reaches the value 10 first then display the message 
+      petAlive.innerText = "Your pet died of hunger. ";
+    // if the value of boredom reaches the value 10 first then display the message 
+    petAlive.innerText = " Tama died of hunger. ";
+    document.getElementById("tam").src = "images/cry.jpeg"
+    if (hunger == 10 || bore == 10 || sleep == 10)
+      // stop the timer if any of the above condition satsfies
+      clearInterval(hungerCounter);
+    // time interval is set as .5 seconds
+  }, 500);
+
+}
+
+// function to increment the boredom
+
+function boreCount() {
+  boreCounter = setInterval(function () {
+    bore++;
+    bone.innerText = "Bore: " + bore;
+    if (bore == 20)
+      boredom.innerText = "Boredom: " + bore;
+    if (bore == 6)
+      // if the value of bore reaches the value 10 first then display the message 
+      petAlive.innerText = "Your pet died of boredom. ";
+    document.getElementById("tam").src = "images/sleepy.jpeg"
+    if (hunger == 20 || bore == 15 || sleep == 15)
+      petAlive.innerText = "Tama died of boredom. ";
+
+    if (hunger == 7 || bore == 10 || sleep == 4)
+      // stop the timer if any of the above condition satsfies
+      clearInterval(boreCounter);
+    // time interval is set as 1 second
+  }, 3000);
+
+}
+
+// function to increment the sleep
+
+function sleepCount() {
+  sleepCounter = setInterval(function () {
+    sleep++;
+    rest.innerText = "Rest: " + sleep;
+    if (sleep == 20)
+      sleepiness.innerText = "Sleepiness: " + sleep;
+    if (sleep == 10)
+      // if the value of bore reaches the value 10 first then display the message 
+
+      petAlive.innerText = "Your pet is tired.";
+    if (hunger == 10 || bore == 10 || sleep == 10)
+      petAlive.innerText = " Tama is tired.";
+    document.getElementById("tam").src = "images/sleepy.jpeg"
+    if (hunger == 3 || bore == 8 || sleep == 10)
+
+      // stop the timer if any of the above condition satsfies
+      clearInterval(sleepCounter);
+    // time interval is set as 2 seconds
+  }, 2000);
+}
+
+// decrement the value of hunger when the feed button is pressed
 
 function feedClick() {
   hunger--;
@@ -108,16 +138,21 @@ function feedClick() {
 };
 
 
+
 function sleepClick() {
   sleep--;
+  rest.innerText = "Rest: " + sleep;
   sleepiness.innerText = "Sleepiness: " + sleep;
 };
 
 
+
 function playClick() {
   bore--;
+  bone.innerText = "Bore: " + bore;
   boredom.innerText = "Boredom: " + bore;
 };
+
 
 
 function resetClick() {
@@ -126,9 +161,11 @@ function resetClick() {
   sleep = 0
   agePet = 0
   hungry.innerText = "Hunger: " + hunger;
+  bone.innerText = "Bore: " + bore;
+  rest.innerText = "Rest: " + sleep;
   boredom.innerText = "Boredom: " + bore;
   sleepiness.innerText = "Sleepiness: " + sleep;
   age.innerText = "Age: " + agePet;
   // recursive call of main function to play the game infinitely
-  main()
+myName()
 };
